37797cb502db0941b804cbddd239d62a9ced891a
Svale Fossåskaret <svale@3by5.no>
2017-09-14 12:58:13 +0200

    Revises test

--- test/specs/ImageUploader.spec.js
+++ test/specs/ImageUploader.spec.js
@@ -1,11 +1,13 @@
 import ImageUploader from 'src/ImageUploader.vue'
 import { createVM } from '../helpers/utils.js'
 
-describe('ImageUploader.vue', function () {
-  it('should render correct contents', function () {
-    const vm = createVM(this, `
-<image-uploader></image-uploader>
-`, { components: { ImageUploader }})
-    vm.$el.querySelector('.upload-caption').textContent.should.eql('Legg til et bilde…')
+describe('ImageUploader', () => {
+  it('Should have a create method', function () {
+    ImageUploader.created.should.be.a('function')
+  })
+
+  it('Should have file type input in dom', function () {
+    const vm = createVM(this, `<image-uploader></image-uploader>`, { components: { ImageUploader }})
+    vm.$el.querySelector('#fileInput').should.have.attr('type', 'file')
   })
 })
