06271ab142f7e1715c8c1f8f6b59cee8a05f5091
Svale Fossåskaret <svale@3by5.no>
2017-11-20 13:19:19 +0100

    Update dist

--- dist/vue-image-upload-resize.esm.js
+++ dist/vue-image-upload-resize.esm.js
@@ -1,5 +1,5 @@
 /*!
- * vue-image-upload-resize v1.1.2
+ * vue-image-upload-resize v1.1.4
  * Based on ImageUploader (c) Ross Turner (https://github.com/rossturner/HTML5-ImageUploader)
  * Adapted by (c) 2017 Svale Fossåskaret
  * Released under the MIT License.
@@ -515,6 +515,6 @@ if (typeof window !== 'undefined' && window.Vue) {
   window.Vue.use(plugin);
 }
 
-var version = '1.1.2';
+var version = '1.1.4';
 
 export { ImageUploader, version };export default plugin;
