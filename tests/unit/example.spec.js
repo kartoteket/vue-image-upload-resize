3379efb360992c25ffed6941edfe7aeaa422c57c
Svale Fossåskaret <svale@3by5.no>
2019-08-07 21:05:48 +0200

    Fix new lint errors after update

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,5 +1,5 @@
 import { shallowMount } from '@vue/test-utils'
-import App from '@/App.vue'
+//import App from '@/App.vue'
 import ImageUploader from '@/components/ImageUploader.vue'
 
 const createCmp = propsData => shallowMount(ImageUploader, { propsData })
