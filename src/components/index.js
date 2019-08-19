3cd09e04dc1425a925e54f2194775a71cd64db4f
Svale Fossåskaret <svale@3by5.no>
2019-08-19 07:38:40 +0200

    Bump (and clear) version tag in example

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 /*!
- * vue-image-upload-resize v2.1.0
+ * vue-image-upload-resize
  * Based on ImageUploader (c) Ross Turner (https://github.com/rossturner/HTML5-ImageUploader)
  * Adapted by (c) 2018 Svale Fossåskaret (http://kartoteket.as/team/svale.html / @Fossesvale)
  * @license MIT.
