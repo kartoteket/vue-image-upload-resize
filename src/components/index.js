a82830d0f1d01a019484ab01eb18c0d9dd5d26d0
Svale Fossåskaret <svale@3by5.no>
2020-01-27 12:05:16 +0100

    Re-factor wrapper script to expose both as plugin and SFC

--- src/components/index.js
+++ src/components/index.js
@@ -4,18 +4,35 @@
  * Adapted by (c) 2018 Svale Fossåskaret (http://kartoteket.as/team/svale.html / @Fossesvale)
  * @license MIT.
  */
-import ImageUploader from './ImageUploader.vue'
+// Import vue component
+import component from './ImageUploader.vue'
 
-const ImageUploaderPlugin = {
-  install(Vue) {
-    Vue.component('image-uploader', ImageUploader)
-  },
+// Declare install function executed by Vue.use()
+export function install(Vue) {
+  if (install.installed) return
+  install.installed = true
+  Vue.component('ImageUploader', component)
 }
 
-// Install by default if using the script tag
-if (typeof window !== 'undefined' && window.Vue) {
-  window.Vue.use(ImageUploaderPlugin)
+// Create module definition for Vue.use()
+const plugin = {
+  install,
 }
 
-export default ImageUploaderPlugin
-export { ImageUploader }
+// Auto-install when vue is found (eg. in browser via <script> tag)
+let GlobalVue = null
+if (typeof window !== 'undefined') {
+  GlobalVue = window.Vue
+} else if (typeof global !== 'undefined') {
+  GlobalVue = global.Vue
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin)
+}
+
+// Inject install function into component - allows component
+// to be registered via Vue.use() as well as Vue.component()
+component.install = install
+
+// To allow use as module (npm/webpack/etc.) export component
+export default component
